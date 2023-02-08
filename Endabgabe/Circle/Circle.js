2e8022b838ddc700b564111eb5af1dcbcba00f11
Skytecc <101884474+Skytecc@users.noreply.github.com>
2023-02-08 02:32:36 +0100

    Add Testcode alleine 02

--- Endabgabe/Circle/Circle.js
+++ Endabgabe/Circle/Circle.js
@@ -2,9 +2,21 @@
 var Feuerwerk;
 (function (Feuerwerk) {
     class Circle extends Feuerwerk.Rocket {
-        constructor(_position, _name, _color1, _color2) {
+        // dx, dy are the destination x and y position 
+        radius;
+        dx;
+        dy;
+        constructor(_position, _name, _color1, _color2, _radius, _dx, _dy) {
             super(_position, _name, _color1, _color2);
+            this.radius = _radius;
+            this.dx = _dx;
+            this.dy = _dy;
+        }
+        update() {
             this.draw();
+            this.alpha -= 0.01;
+            this.position.x += this.dx;
+            this.position.y += this.dy;
         }
         draw() {
             Feuerwerk.crc2.save();
@@ -14,6 +26,7 @@ var Feuerwerk;
         }
         drawArc(_x, _y, _radius, _startAngle, _endAngle, _color) {
             Feuerwerk.crc2.beginPath();
+            Feuerwerk.crc2.globalAlpha = this.alpha;
             Feuerwerk.crc2.arc(_x, _y, _radius, _startAngle, _endAngle * Math.PI);
             Feuerwerk.crc2.fillStyle = _color;
             Feuerwerk.crc2.fill();
--- Endabgabe/Circle/Circle.ts
+++ Endabgabe/Circle/Circle.ts
@@ -1,10 +1,29 @@
 namespace Feuerwerk {
     export class Circle extends Rocket {
 
-        constructor(_position: Vector, _name: string, _color1: string, _color2: string) {
+        // dx, dy are the destination x and y position 
+
+        private radius: number;
+        
+        private dx: number;
+
+        private dy: number;
+        
+        constructor(_position: Vector, _name: string, _color1: string, _color2: string, _radius: number, _dx: number, _dy: number) {
             super(_position, _name, _color1, _color2);
 
+            this.radius = _radius;
+            this.dx = _dx;
+            this.dy = _dy;
+        }
+
+        public update(): void {
+
             this.draw();
+            this.alpha -= 0.01;
+            this.position.x += this.dx;
+            this.position.y += this.dy;
+
 
         }
 
@@ -23,6 +42,7 @@ namespace Feuerwerk {
 
         public drawArc(_x: number, _y: number, _radius: number, _startAngle: number, _endAngle: number, _color: string): void {
             crc2.beginPath();
+            crc2.globalAlpha = this.alpha;
             crc2.arc(_x, _y, _radius, _startAngle, _endAngle * Math.PI);
             crc2.fillStyle = _color;
             crc2.fill();
