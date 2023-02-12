7a5096d0b8298f103f550b0db7c962992c530d8d
Skytecc <101884474+Skytecc@users.noreply.github.com>
2023-02-12 17:13:23 +0100

    Renew Star

--- Endabgabe/Star/star.js
+++ Endabgabe/Star/star.js
@@ -8,23 +8,24 @@ var Feuerwerk;
         draw() {
             Feuerwerk.crc2.save();
             Feuerwerk.crc2.translate(-20, 180);
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170, this.color); // oben
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 0, this.position.y - 155, this.position.x + 22.5, this.position.y - 170, this.color); // unten links
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 30, this.position.y - 155, this.position.x + 22.5, this.position.y - 170, this.color); // unten rechts
-            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x - 7.5, this.position.y - 180, this.color); // links
-            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x + 35, this.position.y - 180, this.color); // rechts 
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170); // oben
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170); // oben
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 0, this.position.y - 155, this.position.x + 22.5, this.position.y - 170); // unten links
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 30, this.position.y - 155, this.position.x + 22.5, this.position.y - 170); // unten rechts
+            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x - 7.5, this.position.y - 180); // links
+            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x + 35, this.position.y - 180); // rechts 
             Feuerwerk.crc2.restore();
         }
         explode() {
             super.explode();
         }
-        drawTriangle(_x1, _y1, _x2, _y2, _x3, _y3, _color) {
+        drawTriangle(_x1, _y1, _x2, _y2, _x3, _y3) {
             Feuerwerk.crc2.beginPath();
             Feuerwerk.crc2.globalAlpha = this.alpha;
             Feuerwerk.crc2.moveTo(_x1, _y1);
             Feuerwerk.crc2.lineTo(_x2, _y2);
             Feuerwerk.crc2.lineTo(_x3, _y3);
-            Feuerwerk.crc2.fillStyle = _color;
+            Feuerwerk.crc2.fillStyle = this.color;
             Feuerwerk.crc2.fill();
             Feuerwerk.crc2.closePath();
         }
--- Endabgabe/Star/star.ts
+++ Endabgabe/Star/star.ts
@@ -1,18 +1,19 @@
 namespace Feuerwerk {
     export class Star extends Rocket {
 
-        constructor(_position: Vector, _dx: number, _dy: number, _alphaTime: number, _name: string, _color: string) {
+        constructor(_position: Vector, _dx: number, _dy: number, _alphaTime: number , _name: string, _color: string) {
             super(_position, _dx, _dy, _alphaTime , _name, _color);
         }
 
         public draw(): void {
             crc2.save();
             crc2.translate(-20, 180);
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170, this.color); // oben
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 0, this.position.y - 155, this.position.x + 22.5, this.position.y - 170, this.color); // unten links
-            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 30, this.position.y - 155, this.position.x + 22.5, this.position.y - 170, this.color); // unten rechts
-            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x - 7.5, this.position.y - 180, this.color); // links
-            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x + 35, this.position.y - 180, this.color); // rechts 
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170); // oben
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 15, this.position.y - 190, this.position.x + 22.5, this.position.y - 170); // oben
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 0, this.position.y - 155, this.position.x + 22.5, this.position.y - 170); // unten links
+            this.drawTriangle(this.position.x + 7.5, this.position.y - 170, this.position.x + 30, this.position.y - 155, this.position.x + 22.5, this.position.y - 170); // unten rechts
+            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x - 7.5, this.position.y - 180); // links
+            this.drawTriangle(this.position.x + 15, this.position.y - 175, this.position.x + 15, this.position.y - 165, this.position.x + 35, this.position.y - 180); // rechts 
             crc2.restore();
         }
 
@@ -20,16 +21,15 @@ namespace Feuerwerk {
             super.explode();
         }
 
-        public drawTriangle(_x1: number, _y1: number, _x2: number, _y2: number, _x3: number, _y3: number, _color: string): void {
+        public drawTriangle(_x1: number, _y1: number, _x2: number, _y2: number, _x3: number, _y3: number): void {
             crc2.beginPath();
             crc2.globalAlpha = this.alpha;
             crc2.moveTo(_x1, _y1);
             crc2.lineTo(_x2, _y2);
             crc2.lineTo(_x3, _y3);
-            crc2.fillStyle = _color;
+            crc2.fillStyle = this.color;
             crc2.fill();
             crc2.closePath();
         }
-
     }
 }
